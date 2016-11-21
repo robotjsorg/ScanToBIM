a4d5513101012c37b9c13a1892c3c88c031e71cf
Joe McMahon <jmcmahon443@gmail.com>
2016-11-21 15:34:05 -0500

    init ScanToBIM

---
+++ ScanToBIM/app/src/main/java/com/example/vulcandocs/scantobim/ScanToBIM.java
@@ -0,0 +1,13 @@
+package com.example.vulcandocs.scantobim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScanToBIM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_to_bim);
+    }
+}
