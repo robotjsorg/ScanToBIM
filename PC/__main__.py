a123013e98391b3cee259a24dbe8268e2cfac03b
Joe McMahon <jmcmahon443@gmail.com>
2016-11-30 13:21:49 -0500

    server works with __main__.py

---
+++ PC/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import os
+
+def main():
+	os.system('javac TCPServer.java')
+	os.system('java TCPServer')
+	os.system('python reader.py Scans/')
+	os.system('python concat.py')
+
+if __name__ == "__main__":
+    main()
--- server.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system('javac PC/TCPServer.java')
-os.system('java PC/TCPServer')
-os.system('python PC/reader.py Scans/')
-os.system('python PC/ concat.py')
