704ed74e459b8fe20ff13aadd038638a3cb59d0e
Joseph McMahon <jmcmahon443@gmail.com>
2016-12-03 09:38:44 -0500

    config to joe's MacBook air

--- PC/concat.py
+++ PC/concat.py
@@ -1,8 +1,8 @@
 import glob
 
-read_files = glob.glob("/home/suriya/Scans/*.xyz")
+read_files = glob.glob("~/Documents/vulcan-docs/Scans/*.xyz")
 
-with open("result.xyz", "wb") as outfile:
+with open("~/Documents/vulcan-docs/Scans/result.xyz", "wb") as outfile:
     for f in read_files:
         with open(f, "rb") as infile:
             outfile.write(infile.read())
